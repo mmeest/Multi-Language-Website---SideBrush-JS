f1d7a1d9307833bf72a93ada2bc10e58e197db27
mmeest <34022590+mmeest@users.noreply.github.com>
2020-05-01 19:52:39 +0300

    Initial

---
+++ translate.js
@@ -0,0 +1,132 @@
+var arrLang = new Array();
+        arrLang['est'] = new Array();
+        arrLang['eng'] = new Array();
+        arrLang['ger'] = new Array();
+        arrLang['fin'] = new Array();
+    
+        // Estonian language
+        arrLang['est']['head'] = 'Külgharja programm';
+        arrLang['est']['labelA'] = 'Põhimuutujad';
+        arrLang['est']['labelB'] = 'Valikulised muutujad';
+        arrLang['est']['labelC'] = 'Väljund';
+        arrLang['est']['legendA'] = 'Nõutud lähteandmed:';
+        arrLang['est']['legendB'] = 'Sektori lähteandmed';
+        arrLang['est']['legendC'] = 'Valikulised lähteandmed:';
+        arrLang['est']['variableA'] = 'A: ketta/sektori läbimõõt(mm)';
+        arrLang['est']['variableB'] = 'B: auguridade arv';
+        arrLang['est']['variableC'] = 'C: kinnituspoltide arv';
+        arrLang['est']['variableD'] = 'D: poltide rea läbimõõt(mm)';
+        arrLang['est']['variableE'] = 'E: sektori algusnurk';
+        arrLang['est']['variableF'] = 'F: sektori lõppnurk';
+        arrLang['est']['variableG'] = 'G: esimese poldi nurk';
+        arrLang['est']['variableH'] = 'H: teise poldi nurk';
+        arrLang['est']['variableI'] = 'I: augupaaride vahe';
+        arrLang['est']['variableJ'] = 'J: ketta ja välimise aukude rea vahe';
+        arrLang['est']['variableK'] = 'K: puuritavate auguridade vahe';
+        arrLang['est']['variableL'] = 'L: augurea alguse kaugus kinnituspoldist';
+        arrLang['est']['variableM'] = 'M: poldirea kaugus sellest väiksemast augureast';
+        arrLang['est']['variableN'] = 'N: poldirea kaugus sellest suuremast augureast';
+        arrLang['est']['defaultA'] = '(vaikimisi 40 mm)';
+        arrLang['est']['defaultB'] = '(vaikimisi 60 mm)';
+        arrLang['est']['defaultC'] = '(vaikimisi 40 mm)';
+        arrLang['est']['defaultD'] = '(vaikimisi 30 mm)';
+        arrLang['est']['defaultE'] = '(vaikimisi 64 mm)';
+        arrLang['est']['defaultF'] = '(vaikimisi 35 mm)';
+    
+        // English language
+        arrLang['eng']['head'] = 'Side brush program';
+        arrLang['eng']['labelA'] = 'Main variables';
+        arrLang['eng']['labelB'] = 'Optional';
+        arrLang['eng']['labelC'] = 'Output';
+        arrLang['eng']['legendA'] = 'Mandatory values:';
+        arrLang['eng']['legendB'] = 'Sector values:';
+        arrLang['eng']['legendC'] = 'Optional values:';
+        arrLang['eng']['variableA'] = 'A: disc/sector diameter(mm)';
+        arrLang['eng']['variableB'] = 'B: number of drilling rows';
+        arrLang['eng']['variableC'] = 'C: number of fixing bolts';
+        arrLang['eng']['variableD'] = 'D: diameter of fixing bolts row(mm)';
+        arrLang['eng']['variableE'] = 'E: sector beginning angle';
+        arrLang['eng']['variableF'] = 'F: sector end angle';
+        arrLang['eng']['variableG'] = 'G: angle of first bolt';
+        arrLang['eng']['variableH'] = 'H: angle of second bolt';
+        arrLang['eng']['variableI'] = 'I: hole pairs distance';
+        arrLang['eng']['variableJ'] = 'J: outer row distance from disc edge';
+        arrLang['eng']['variableK'] = 'K: distance between drilling rows';
+        arrLang['eng']['variableL'] = 'L: holes row start distance from fixing bolt';
+        arrLang['eng']['variableM'] = 'M: bolt row distance from smaller drilling row';
+        arrLang['eng']['variableN'] = 'N: bolt row distance from bigger drilling row';
+        arrLang['eng']['defaultA'] = '(default 40 mm)';
+        arrLang['eng']['defaultB'] = '(default 60 mm)';
+        arrLang['eng']['defaultC'] = '(default 40 mm)';
+        arrLang['eng']['defaultD'] = '(default 30 mm)';
+        arrLang['eng']['defaultE'] = '(default 64 mm)';
+        arrLang['eng']['defaultF'] = '(default 35 mm)';
+
+        // German language
+        arrLang['ger']['head'] = 'Seitenbürstenprogramm';
+        arrLang['ger']['labelA'] = 'Hauptvariablen';
+        arrLang['ger']['labelB'] = 'Optional';
+        arrLang['ger']['labelC'] = 'Ausgabe';
+        arrLang['ger']['legendA'] = 'Obligatorische Werte:';
+        arrLang['ger']['legendB'] = 'Sektorwerte:';
+        arrLang['ger']['legendC'] = 'Optionale Werte:';
+        arrLang['ger']['variableA'] = 'A: Scheiben-/Sektordurchmesser(mm)';
+        arrLang['ger']['variableB'] = 'B: Anzahl der Bohrreihen';
+        arrLang['ger']['variableC'] = 'C: Anzahl der Befestigungsschrauben';
+        arrLang['ger']['variableD'] = 'D: Durchmesser der Befestigungsschraubenreihe(mm)';
+        arrLang['ger']['variableE'] = 'E: Sektoranfangswinkel';
+        arrLang['ger']['variableF'] = 'F: Sektorendwinkel';
+        arrLang['ger']['variableG'] = 'G: Winkel der ersten Schraube';
+        arrLang['ger']['variableH'] = 'H: Winkel der zweiten Schraube';
+        arrLang['ger']['variableI'] = 'I: Lochpaare Abstand';
+        arrLang['ger']['variableJ'] = 'J: äußerer Reihenabstand von der Scheibenkante';
+        arrLang['ger']['variableK'] = 'K: Abstand zwischen den Bohrreihen';
+        arrLang['ger']['variableL'] = 'L: Löcher Reihe Startabstand von Befestigungsschraube';
+        arrLang['ger']['variableM'] = 'M: Schraubenreihenabstand von der kleineren Bohrreihe';
+        arrLang['ger']['variableN'] = 'N: Schraubenreihenabstand von größerer Bohrreihe';
+        arrLang['ger']['defaultA'] = '(Standard 40 mm)';
+        arrLang['ger']['defaultB'] = '(Standard 60 mm)';
+        arrLang['ger']['defaultC'] = '(Standard 40 mm)';
+        arrLang['ger']['defaultD'] = '(Standard 30 mm)';
+        arrLang['ger']['defaultE'] = '(Standard 64 mm)';
+        arrLang['ger']['defaultF'] = '(Standard 35 mm)';
+
+        // Finnish language
+        arrLang['fin']['head'] = 'Kylkiharjan ohjelma';
+        arrLang['fin']['labelA'] = 'Päämuuttujat';
+        arrLang['fin']['labelB'] = 'Valinnainen';
+        arrLang['fin']['labelC'] = 'Ulostulo';
+        arrLang['fin']['legendA'] = 'Pakolliset arvot:';
+        arrLang['fin']['legendB'] = 'Sektoriarvot:';
+        arrLang['fin']['legendC'] = 'Valinnaiset arvot:';
+        arrLang['fin']['variableA'] = 'A: levyn/sektorin halkaisija(mm)';
+        arrLang['fin']['variableB'] = 'B: porausrivien lukumäärä';
+        arrLang['fin']['variableC'] = 'C: kiinnityspulttien lukumäärä';
+        arrLang['fin']['variableD'] = 'D: kiinnityspultin rivin halkaisija(mm)';
+        arrLang['fin']['variableE'] = 'E: sektorin alkamiskulma';
+        arrLang['fin']['variableF'] = 'F: sektorin loppukulma';
+        arrLang['fin']['variableG'] = 'G: ensimmäisen pultin kulma';
+        arrLang['fin']['variableH'] = 'H: toisen pultin kulma';
+        arrLang['fin']['variableI'] = 'I: reikäparien etäisyys';
+        arrLang['fin']['variableJ'] = 'J: ulomman rivin etäisyys levyn reunasta';
+        arrLang['fin']['variableK'] = 'K: porausrivien välinen etäisyys';
+        arrLang['fin']['variableL'] = 'L: reikien rivin aloitusetäisyys kiinnityspultista';
+        arrLang['fin']['variableM'] = 'M: pulttirivin etäisyys pienemmästä porausrivistä';
+        arrLang['fin']['variableN'] = 'N: pulttirivin etäisyys suuremmasta porausrivistä';
+        arrLang['fin']['defaultA'] = '(oletusarvo 40 mm)';
+        arrLang['fin']['defaultB'] = '(oletusarvo 60 mm)';
+        arrLang['fin']['defaultC'] = '(oletusarvo 40 mm)';
+        arrLang['fin']['defaultD'] = '(oletusarvo 30 mm)';
+        arrLang['fin']['defaultE'] = '(oletusarvo 64 mm)';
+        arrLang['fin']['defaultF'] = '(oletusarvo 35 mm)';
+    
+        // Process translation
+        $(function() {
+          $('.translate').click(function() {
+            var lang = $(this).attr('id');
+    
+            $('.lang').each(function(index, item) {
+              $(this).text(arrLang[lang][$(this).attr('key')]);
+            });
+          });
+        });
